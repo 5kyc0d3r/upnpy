f0996e0d340f6614a6b116c192fca6033c70af64
5kyc0d3r <skycoder.official@protonmail.com>
2019-04-18 17:27:15 -0300

    Added function for retrieving device services

--- upnpy/ssdp/SSDPDevice.py
+++ upnpy/ssdp/SSDPDevice.py
@@ -1,6 +1,17 @@
 import urllib.request
+from xml.dom import minidom
+
 import upnpy.utils as utils
 from upnpy.ssdp import SSDPFilters
+
+
+class DeviceService:
+    def __init__(self, service_type, service_id, scpd_url, control_url, event_sub_url):
+        self.service_type = service_type
+        self.service_id = service_id
+        self.scpd_url = scpd_url
+        self.control_url = control_url
+        self.event_sub_url = event_sub_url
 
 
 class SSDPDevice:
@@ -19,13 +30,38 @@ class SSDPDevice:
         self.host = address[0]
         self.port = address[1]
         self.response = response
+
         self.device_description = None
+        self.device_services = []
 
     def get_device_description(self):
         device_description_url = utils.parse_http_header(self.response, 'Location')
         device_description = urllib.request.urlopen(device_description_url).read()
         self.device_description = device_description.decode()
         return self.device_description
+
+    def get_device_services(self):
+        while True:
+            if self.device_description:
+                root = minidom.parseString(self.device_description)
+
+                for service in root.getElementsByTagName('service'):
+                    self.device_services.append(
+                        DeviceService(
+                            service_type=service.getElementsByTagName('serviceType')[0].firstChild.nodeValue,
+                            service_id=service.getElementsByTagName('serviceId')[0].firstChild.nodeValue,
+                            scpd_url=service.getElementsByTagName('SCPDURL')[0].firstChild.nodeValue,
+                            control_url=service.getElementsByTagName('controlURL')[0].firstChild.nodeValue,
+                            event_sub_url=service.getElementsByTagName('eventSubURL')[0].firstChild.nodeValue
+                        )
+                    )
+            else:
+                self.get_device_description()
+                continue
+
+            break
+
+        return self.device_services
 
     @staticmethod
     def filter_by(devices, **filters):
