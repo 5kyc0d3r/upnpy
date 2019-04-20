6b705e25a16c9a6e401ab4e957adab5ccb362d65
5kyc0d3r <skycoder.official@protonmail.com>
2019-04-20 12:18:25 -0300

    Added Action class for storing SOAP action data

---
+++ upnpy/soap/Action.py
@@ -0,0 +1,11 @@
+class SOAPAction:
+    def __init__(self, name, argument_list):
+        self.name = name
+        self.argument_list = argument_list
+
+    class Argument:
+        def __init__(self, name, direction, return_value, related_state_variable):
+            self.name = name
+            self.direction = direction
+            self.return_value = return_value
+            self.related_state_variable = related_state_variable
