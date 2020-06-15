45e3d06404bd1d063099836cdbd41d55ce3e6d16
5kyc0d3r <skycoder.official@protonmail.com>
2020-06-15 18:56:41 -0300

    Added new py version

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     url=__project_url__,
     license='MIT',
